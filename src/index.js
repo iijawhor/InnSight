a47791de7d9a2b51042e1652ad1fb6a3d6aba1f0
iijawhor <jawhorali1@gmail.com>
2024-02-13 00:59:46 +0530

    removed unused imports

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { SearchPage, HomePage } from "./exports/export";
+// import { SearchPage, HomePage } from "./exports/export";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const router = createBrowserRouter([
   {
