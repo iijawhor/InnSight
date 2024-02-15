8d5db0e8dc786556b866964c73f6ade661dcf9c8
iijawhor <jawhorali1@gmail.com>
2024-02-15 22:57:34 +0530

    added files and folders to ecport

--- src/exports/export.js
+++ src/exports/export.js
@@ -5,6 +5,8 @@ import HomePage from "../pages/HomePage/HomePage";
 import Logo from "../components/Logo/Logo";
 import MenuIcon from "@mui/icons-material/Menu";
 import LanguageIcon from "@mui/icons-material/Language";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import GradeIcon from "@mui/icons-material/Grade";
 import SearchIcon from "@mui/icons-material/Search";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import NavbarMenu from "../components/NavbarMenu/NavbarMenu";
@@ -13,6 +15,9 @@ import SearchPage from "../pages/SearchPage/SearchPage";
 import InputComponent from "../components/InputComponent/InputComponent";
 import fetchFromAPI from "../FetchFromAPI/fetchFromAPI";
 import SearchResults from "../components/SearchResults/SearchResults";
+import Card from "../components/Card/Card";
+import { setSearchData } from "../store/searchResultSlice";
+import CurrencyRupeeIcon from "@mui/icons-material/CurrencyRupee";
 export {
   Container,
   HomePage,
@@ -21,12 +26,17 @@ export {
   Logo,
   MenuIcon,
   LanguageIcon,
+  GradeIcon,
   SearchIcon,
   AccountCircleIcon,
+  FavoriteBorderIcon,
   NavbarMenu,
   SearchComponent,
   SearchPage,
   InputComponent,
   fetchFromAPI,
-  SearchResults
+  SearchResults,
+  Card,
+  setSearchData,
+  CurrencyRupeeIcon
 };
